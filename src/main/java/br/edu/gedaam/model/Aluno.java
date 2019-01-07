db82eae3203a66635cefc464301a3ee013cc91ec
Raquel Lemos <raqlem@gmail.com>
2019-01-07 09:19:02 -0200

    Back to lombok in Aluno

--- src/main/java/br/edu/gedaam/model/Aluno.java
+++ src/main/java/br/edu/gedaam/model/Aluno.java
@@ -3,15 +3,14 @@ package br.edu.gedaam.model;
 //import lombok.*;
 
 
+import lombok.Data;
+
 import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity
 @Table(name="alunos")
-//@Getter
-//@Setter
-//@ToString
-//@RequiredArgsConstructor
+@Data
 public class Aluno  implements Serializable {
 
     private static final long serialVersionUID = -5627354148312027848L;
@@ -31,45 +30,5 @@ public class Aluno  implements Serializable {
 
     public static long getSerialVersionUID() {
         return serialVersionUID;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getFaculdade() {
-        return faculdade;
-    }
-
-    public void setFaculdade(String faculdade) {
-        this.faculdade = faculdade;
-    }
-
-    public String getMatricula() {
-        return matricula;
-    }
-
-    public void setMatricula(String matricula) {
-        this.matricula = matricula;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
     }
 }
