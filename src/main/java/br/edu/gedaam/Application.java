e76a4f51c046ce15f472e1c62bb128c214a0b469
Raquel Lemos <raqlem@gmail.com>
2019-02-03 18:23:35 -0200

    Waking up method

--- src/main/java/br/edu/gedaam/Application.java
+++ src/main/java/br/edu/gedaam/Application.java
@@ -1,12 +1,34 @@
 package br.edu.gedaam;
 
+import br.edu.gedaam.service.EmailService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 @SpringBootApplication
 public class Application {
 
+
+    private static void wakeup(){
+        TimerTask repeatedTask = new TimerTask() {
+            public void run() {
+                System.out.println("Wake up");
+                EmailService emailService = new EmailService();
+                emailService.sendEmail("RaqueL Lemos", "raqlem@gmail.com",
+                        "Wake Up", "Waking up");
+            }
+        };
+        Timer timer = new Timer("Timer");
+
+        long delay  = 1000L*60L*29L;
+        long period = 1000L*60L*29L; //(29mins)
+        timer.scheduleAtFixedRate(repeatedTask, delay, period);
+    }
+
     public static void main(String[] args) {
+        wakeup();
         SpringApplication.run(Application.class, args);
     }
 }
